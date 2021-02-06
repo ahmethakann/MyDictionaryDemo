1000d95e2a72dd85e22258359081160f798570d3
ahmethakann <66576626+ahmethakann@users.noreply.github.com>
2021-02-06 14:27:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyDictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> kişiler = new MyDictionary<int, string>();
+            kişiler.Add(1, "Ali");
+            kişiler.Add(2, "Engin");
+            kişiler.Add(3, "Yavuz");
+            kişiler.Add(4, "Derin");
+            kişiler.Add(5, "Ahmet");
+            kişiler.Add(6, "Ayşe");
+            
+        }
+
+    }
+}
